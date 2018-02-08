2e7d5341200c1c044e7c5329adc918f2a799a00a
Kevin Rutherford <kevin@rutherford-software.com>
2018-02-08 12:29:55 +0000

    Copy in the files

---
+++ java/src/com/xpsurgery/trivia/uglytrivia/Game.java
@@ -0,0 +1,121 @@
+package com.xpsurgery.trivia.uglytrivia;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Game {
+  ArrayList players = new ArrayList();
+  int[] places = new int[6];
+  int[] purses  = new int[6];
+  boolean[] inPenaltyBox  = new boolean[6];
+  LinkedList popQuestions = new LinkedList();
+  LinkedList scienceQuestions = new LinkedList();
+  LinkedList sportsQuestions = new LinkedList();
+  LinkedList rockQuestions = new LinkedList();
+  int currentPlayer = 0;
+  boolean isGettingOutOfPenaltyBox;
+
+  public  Game(){
+    for (int i = 0; i < 50; i++) {
+      popQuestions.addLast("Pop Question " + i);
+      scienceQuestions.addLast(("Science Question " + i));
+      sportsQuestions.addLast(("Sports Question " + i));
+      rockQuestions.addLast(createRockQuestion(i));
+    }
+  }
+
+	public String createRockQuestion(int index){
+		return "Rock Question " + index;
+	}
+
+	public boolean isPlayable() {
+		return (howManyPlayers() >= 2);
+	}
+
+	public boolean add(String playerName) {
+    players.add(playerName);
+    places[howManyPlayers()] = 0;
+    purses[howManyPlayers()] = 0;
+    inPenaltyBox[howManyPlayers()] = false;
+		return true;
+	}
+
+	public int howManyPlayers() {
+		return players.size();
+	}
+
+	public void roll(int roll) {
+		if (inPenaltyBox[currentPlayer]) {
+			if (roll % 2 != 0) {
+				isGettingOutOfPenaltyBox = true;
+				places[currentPlayer] = places[currentPlayer] + roll;
+				if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
+				askQuestion();
+			} else {
+				isGettingOutOfPenaltyBox = false;
+      }
+		} else {
+			places[currentPlayer] = places[currentPlayer] + roll;
+			if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
+			askQuestion();
+		}
+	}
+
+	private void askQuestion() {
+		if (currentCategory() == "Pop")
+			System.out.println(popQuestions.removeFirst());
+		if (currentCategory() == "Science")
+			System.out.println(scienceQuestions.removeFirst());
+		if (currentCategory() == "Sports")
+			System.out.println(sportsQuestions.removeFirst());
+		if (currentCategory() == "Rock")
+			System.out.println(rockQuestions.removeFirst());
+	}
+
+	private String currentCategory() {
+		if (places[currentPlayer] == 0) return "Pop";
+		if (places[currentPlayer] == 4) return "Pop";
+		if (places[currentPlayer] == 8) return "Pop";
+		if (places[currentPlayer] == 1) return "Science";
+		if (places[currentPlayer] == 5) return "Science";
+		if (places[currentPlayer] == 9) return "Science";
+		if (places[currentPlayer] == 2) return "Sports";
+		if (places[currentPlayer] == 6) return "Sports";
+		if (places[currentPlayer] == 10) return "Sports";
+		return "Rock";
+	}
+
+	public boolean wasCorrectlyAnswered() {
+		if (inPenaltyBox[currentPlayer]){
+			if (isGettingOutOfPenaltyBox) {
+				purses[currentPlayer]++;
+				boolean winner = didPlayerWin();
+				currentPlayer++;
+				if (currentPlayer == players.size()) currentPlayer = 0;
+				return winner;
+			} else {
+				currentPlayer++;
+				if (currentPlayer == players.size()) currentPlayer = 0;
+				return true;
+			}
+		} else {
+			purses[currentPlayer]++;
+			boolean winner = didPlayerWin();
+			currentPlayer++;
+			if (currentPlayer == players.size()) currentPlayer = 0;
+			return winner;
+		}
+	}
+
+	public boolean wrongAnswer(){
+		inPenaltyBox[currentPlayer] = true;
+		currentPlayer++;
+		if (currentPlayer == players.size()) currentPlayer = 0;
+		return true;
+	}
+
+	private boolean didPlayerWin() {
+		return !(purses[currentPlayer] == 6);
+	}
+}
+
